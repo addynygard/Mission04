2a4ea27e4003b8e24dd7c7775737aa4903fdd71f
Addy <dragyn.addy@gmail.com>
2025-01-29 13:30:29 -0700

    added method to print array/tic tac toe board

--- Receives.cs
+++ Receives.cs
@@ -8,6 +8,25 @@ namespace Mission04
 {
     internal class Receives
     {
+        public static void ReceiveArray(string[] choices)
+        {
+            for (int i = 0; i < choices.Length; i++)
+            {
+                Console.Write(choices[i]);
 
+                if ((i +1) % 3 != 0)
+                {
+                    Console.WriteLine("|");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    if (i < choices.Length - 1)
+                    {
+                        Console.WriteLine("-----");
+                    }
+                }
+            }
+        }
     }
 }
