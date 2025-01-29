01e1f49e7ca78347a4840c2cf628c141d9a7fbd6
is201spencer <scw01@byu.edu>
2025-01-29 13:30:31 -0700

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Tic-Tac-Toe");
+
+List<string> choices = new List<string>();
+int numPlayers = 0;
+
+Console.WriteLine("How many people are playing");
+numPlayers = int.Parse(Console.ReadLine());
+
+
