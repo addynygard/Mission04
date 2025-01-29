481cdb245b10a5f4c1253339aa0328738851155c
ashleebingham <ashleeb9@byu.edu>
2025-01-29 14:05:02 -0700

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Mission04;
+
+Console.WriteLine("Welcome to Tic-Tac-Toe");
+
+List<string> choices = new List<string>();
+int numPlayers = 0;
+Boolean gameOver = false;
+Receives r = new Receives(); //Why error
+string currentPlayer = "X";
+string input = "";
+
+Console.WriteLine("How many people are playing");
+numPlayers = int.Parse(Console.ReadLine());
+
+
+do
+{
+    // print array
+    r.ReceivesArray();
+
+    Console.WriteLine($"Player {currentPlayer}, choose position (1-9).");
+    input = Console.ReadLine();
+
+    if (int.TryParse(input, out int position) && position >= 1 && position <= 9)
+    {
+        choices[position - 1] = currentPlayer;
+        r.CheckWinner(choices); //Which variable
+
+    }
+
+
+
+    else
+    {
+        if (currentPlayer == "X")
+        {
+            currentPlayer = "O";
+        }
+
+        else
+        {
+            currentPlayer = "X";
+        }
+    }
+
+
+
+} while (!gameOver);
